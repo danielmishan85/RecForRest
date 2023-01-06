65f0fbc9f318bc9c93448c6f361ad51d131b2ee5
danielmishan85 <94166330+danielmishan85@users.noreply.github.com>
2023-01-06 18:38:21 +0200

    first activity: visuality of sign in and,sign up top menu for first activity

--- app/src/main/java/com/example/recforrest/MainActivity.java
+++ app/src/main/java/com/example/recforrest/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.recforrest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    NavController navController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.main_nav_host);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupActionBarWithNavController(this,navController);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home){
+            navController.popBackStack();
+        }else{
+            return NavigationUI.onNavDestinationSelected(item,navController);
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
